a939b27423ea9e764ec183ef533eb16e33c21068
Rendy Sutandy <rendy.sutandy@dataon.com>
2017-09-05 19:32:08 +0700

    add new page pmc

---
+++ src/pages/pmc/pmc.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { PmcPage } from './pmc';
+
+@NgModule({
+  declarations: [
+    PmcPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(PmcPage),
+  ],
+  exports: [
+    PmcPage
+  ]
+})
+export class PmcPageModule {}
