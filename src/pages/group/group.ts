e15a3b7a87c4c4a33ab5653bf427a4cf1ebb26dc
Rendy Sutandy <rendy.sutandy@dataon.com>
2017-09-05 19:29:43 +0700

    add new group page

---
+++ src/pages/group/group.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the GroupPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@IonicPage()
+@Component({
+  selector: 'page-group',
+  templateUrl: 'group.html',
+})
+export class GroupPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    
+  }
+
+}
